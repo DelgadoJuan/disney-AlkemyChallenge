9646ba22a52316c067236db4ae2584a58ccc142c
DelgadoJuan <juandelgado@live.com.ar>
2024-06-28 13:36:03 -0300

    build: proyect structure

---
+++ src/main/java/com/alkemy/disney_AlkemyChallenge/DesafioSobreDisneyDeAlkemyApplication.java
@@ -0,0 +1,13 @@
+package com.alkemy.disney_AlkemyChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSobreDisneyDeAlkemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSobreDisneyDeAlkemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alkemy/disney_AlkemyChallenge/DesafioSobreDisneyDeAlkemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkemy.disney_AlkemyChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSobreDisneyDeAlkemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
